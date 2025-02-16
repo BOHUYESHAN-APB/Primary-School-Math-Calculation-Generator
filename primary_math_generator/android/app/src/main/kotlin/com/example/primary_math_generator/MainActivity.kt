81c693c317aecff03d442c3b810c6a7e5b806bac
XingZhiShengLuo <65199880+BOHUYESHAN-APB@users.noreply.github.com>
2025-02-16 19:24:12 +0800

    完全重构推到再来

--- primary_math_generator/android/app/src/main/kotlin/com/example/primary_math_generator/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.primary_math_generator
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- primary_math_generator/lib/core/src/models/operation_type.dart
+++
@@ -1,1 +0,0 @@
-enum OperationType { addition, subtraction, multiplication, division }
